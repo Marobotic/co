c38027d90bbe1696950e73301805406b3bd8480c
Marobotic <moakamusketeer@gmail.com>
2017-11-28 23:18:28 +0200

    indenting code because Mathy hates it

--- magical_power.js
+++ magical_power.js
@@ -1,24 +1,15 @@
-
-
 $(document).ready(function() {
     $(".button").click(function(event) {
-
-
         $(".text").text("YOU'RE TOO WEAK!");
+        $(this).slideUp(300).delay(3000).fadeIn(400);
         console.log("pressed");
 
-
-        $(this).slideUp(300).delay(3000).fadeIn(400);
-
-  // place this within dom ready function
-  function reset() {     
-    $(".text").text("YOU MUST CRAFT SOME MAGIC TO PASS, PEASANT!");
-    console.log("reset function!");
-    $(".button").text("CRAFT AGAIN!");
- }
-
- // use setTimeout() to execute
- setTimeout(reset, 3000)
+        function reset() {
+            $(".text").text("YOU MUST CRAFT SOME MAGIC TO PASS, PEASANT!");
+            $(".button").text("CRAFT AGAIN!");
+            console.log("reset function!");
+        }
+        setTimeout(reset, 3000)
     });
 
 
